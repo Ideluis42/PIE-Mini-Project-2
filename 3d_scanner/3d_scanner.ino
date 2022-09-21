b50a7791928df674431891e1b5cc7dcd4b7443c3
drewnotdrew <53927275+drewnotdrew@users.noreply.github.com>
2022-09-21 14:09:19 -0400

    Add servo sweep demo into scanning state
    The demo will sweep through the ranges calculated by the bounding box
    calc function.

--- 3d_scanner/3d_scanner.ino
+++ 3d_scanner/3d_scanner.ino
@@ -21,7 +21,7 @@ float t = 0;
 float out;
 
 // User parameters of the 3D scanner and letter
-const float scanner_distance = 0.001; // Distance from the sensor to the letter (m)
+const float scanner_distance = 0.4; // Distance from the sensor to the letter (m)
 const float sensor_height = 0.1; // Distance from the sensor to the ground (m)
 const float letter_height = 0.3556; // Height of the letter to be scanned (m)
 const float letter_width = 0.2286; // Width of the letter to be scanned (m)
@@ -29,9 +29,13 @@ const float letter_tolerance = 0.05; // Tolerance to measure to the sides of the
 const uint16_t scanning_interval = 10; // Interval to wait between servo moves and scans (milliseconds)
 const uint16_t DEBOUNCE_INTERVAL = 10; // Minimum interval to wait before checking button presses 
 float scanning_width_angle; // 
+float scanning_height_lower_angle; //
+float scanning_height_upper_angle; //
 
 // User settings of the 3D scanner
-const uint8_t scanning_resolution = 1; // Angular resolution of the scanner (degrees)
+// const uint8_t scanning_resolution = 1; // Angular resolution of the scanner (degrees) //TODO: Delete if unused
+const uint8_t mesh_points_x = 10; //
+const uint8_t mesh_points_y = 10; //
 
 // Define states for the 3D scanner
 enum states {
@@ -48,13 +52,17 @@ void calculate_bounding_box() {
   // from the letter, calculate the angular bounding box to scan.
   // Get absolute pitch, roll, and yaw from the IMU
   float scanning_height = letter_height + 2 * letter_tolerance;
-  Serial.println(letter_height);
+  //Serial.println(letter_height);
   float scanning_width = letter_width + 2 * letter_tolerance;
-  Serial.println(scanning_width);
+  //Serial.println(scanning_width);
 
   // Calculate the angles corresponding to half of the scanning range
-  float scanning_width_angle = (180*atan((scanning_width/2)/scanner_distance))/PI;
+  scanning_width_angle = (180*atan((scanning_width/2)/scanner_distance))/PI;
+  scanning_height_lower_angle = (180*atan((sensor_height + letter_tolerance)/scanner_distance))/PI;
+  scanning_height_upper_angle = (180*atan((letter_height - sensor_height + letter_tolerance)/scanner_distance))/PI;
   Serial.println(scanning_width_angle);
+  Serial.println(scanning_height_lower_angle);
+  Serial.println(scanning_height_upper_angle);
 }
 
 void idle() {
@@ -100,7 +108,24 @@ void scanning() {
     // Move to top left to begin scanning
   }
   // Scanning code in grid
+  for (float i=-scanning_width_angle; i <= scanning_width_angle; i+=0.01) {
+    pan_servo.write(i+90);
+    //Serial.println(i);
+    delay(1);
+  }
+  for (float i=-scanning_height_upper_angle; i <= scanning_height_lower_angle; i+=0.01) {
+    tilt_servo.write(i+90);
+    //Serial.println(i);
+    delay(1);
+  }
+
+  // Return servo positions to zero upon state exit
+  pan_servo.write(90);
+  tilt_servo.write(90);
+  state = IDLE;
 }
+
+//void scan_width(bool ) {
 
 void setup() {
   Serial.begin(9600);
