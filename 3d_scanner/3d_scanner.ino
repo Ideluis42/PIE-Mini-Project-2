d9475a124f0e736f984e69603bb293ed0cc50218
drewnotdrew <53927275+drewnotdrew@users.noreply.github.com>
2022-09-21 18:37:47 -0400

    Complete servo movement code in scanning state
    Mesh points per axis are tunable, along with the time to wait while
    scanning each mesh point.

--- 3d_scanner/3d_scanner.ino
+++ 3d_scanner/3d_scanner.ino
@@ -27,15 +27,21 @@ const float letter_height = 0.3556; // Height of the letter to be scanned (m)
 const float letter_width = 0.2286; // Width of the letter to be scanned (m)
 const float letter_tolerance = 0.05; // Tolerance to measure to the sides of the letter (m)
 const uint16_t scanning_interval = 10; // Interval to wait between servo moves and scans (milliseconds)
-const uint16_t DEBOUNCE_INTERVAL = 10; // Minimum interval to wait before checking button presses 
+const uint16_t DEBOUNCE_INTERVAL = 1; // Minimum interval to wait before checking button presses 
 float scanning_width_angle; // 
 float scanning_height_lower_angle; //
 float scanning_height_upper_angle; //
+float scanning_width_total_angle; //
+float scanning_height_total_angle; //
+float x_position; //
+float y_position; //
+int current_mesh_points_x;
+int current_mesh_points_y;
 
 // User settings of the 3D scanner
 // const uint8_t scanning_resolution = 1; // Angular resolution of the scanner (degrees) //TODO: Delete if unused
-const uint8_t mesh_points_x = 10; //
-const uint8_t mesh_points_y = 10; //
+const uint8_t mesh_points_x = 25; //
+const uint8_t mesh_points_y = 25; //
 
 // Define states for the 3D scanner
 enum states {
@@ -60,6 +66,8 @@ void calculate_bounding_box() {
   scanning_width_angle = (180*atan((scanning_width/2)/scanner_distance))/PI;
   scanning_height_lower_angle = (180*atan((sensor_height + letter_tolerance)/scanner_distance))/PI;
   scanning_height_upper_angle = (180*atan((letter_height - sensor_height + letter_tolerance)/scanner_distance))/PI;
+  scanning_width_total_angle = scanning_width_angle*2;
+  scanning_height_total_angle = scanning_height_upper_angle + scanning_height_lower_angle;
   Serial.println(scanning_width_angle);
   Serial.println(scanning_height_lower_angle);
   Serial.println(scanning_height_upper_angle);
@@ -100,54 +108,60 @@ void idle() {
   }
 }
 
-void scanning() {
-  // Initialization for scanning state
-  if (state != prior_state) {
-    prior_state = state;
-    calculate_bounding_box();
-  }
-  int current_mesh_points_x = 0;
-  int current_mesh_points_y = 0;
-  current_time = millis();
-  set_position(45,45);
-  delay(1000);
-  set_position(45,-45);
-  delay(1000);
-  set_position(-45,-45);
-  delay(1000);
-  set_position(-45,45);
-  delay(1000);
-  
-//  if (current_time >= prior_time + scanning_interval) {
-//    
-//  }
-  
-//  // Scanning code in grid
-//  for (float i=-scanning_width_angle; i <= scanning_width_angle; i+=0.01) {
-//    pan_servo.write(i+90);
-//    //Serial.println(i);
-//    delay(1);
-//  }
-//  for (float i=-scanning_height_upper_angle; i <= scanning_height_lower_angle; i+=0.01) {
-//    tilt_servo.write(i+90);
-//    //Serial.println(i);
-//    delay(1);
-//  }
-
-
-  // Return servo positions to zero upon state exit
-  pan_servo.write(90);
-  tilt_servo.write(90);
-  //state = IDLE;
-}
-
-//void scan_width(bool ) {
-
 void set_position(float x, float y) {
   // Set the absolute position of the sensor in the angular domain
   pan_servo.write(90-x);
   tilt_servo.write(90-y);
 }
+
+void scanning() {
+  // Initialization for scanning state
+  if (state != prior_state) {
+    prior_state = state;
+    calculate_bounding_box();
+    // Set first point to scan in the top left corner
+    x_position = -scanning_width_angle;
+    y_position = scanning_height_upper_angle;
+    current_mesh_points_x = 0;
+    current_mesh_points_y = 0;
+    set_position(x_position,y_position);
+  }
+  current_time = millis();
+//  Serial.print(current_mesh_points_x);
+//  Serial.print("\t");
+//  Serial.println(current_mesh_points_y);
+  if (current_mesh_points_y <= mesh_points_y) {
+    if (current_mesh_points_x < mesh_points_x-1) {
+      if (current_time >= prior_time + scanning_interval) {
+        prior_time = current_time;
+        // SCAN HERE!
+        current_mesh_points_x += 1;
+        if ((current_mesh_points_y % 2) == 0) {
+          x_position += scanning_width_total_angle/mesh_points_x;
+        } else {
+          x_position -= scanning_width_total_angle/mesh_points_x;
+        }
+        set_position(x_position, y_position);
+      }
+    } else {
+      if (current_time >= prior_time + scanning_interval) {
+        prior_time = current_time;
+        y_position -= scanning_height_total_angle/mesh_points_y;
+        set_position(x_position, y_position);
+        // SCAN HERE!
+        current_mesh_points_y += 1;
+        current_mesh_points_x = 0;
+      }
+    }
+  } else {
+    // Return servo positions to zero upon state exit
+    pan_servo.write(90);
+    tilt_servo.write(90);
+    state = IDLE;
+  }
+}
+
+//void scan_width(bool ) {
 
 void setup() {
   Serial.begin(9600);
