9c120d0f0ec65282bce110f26548d10e82f78e1f
drewnotdrew <53927275+drewnotdrew@users.noreply.github.com>
2022-09-20 23:50:59 -0400

    Add state switching and calc of width scanning
    Idle state fades leds in and out and pushing a button switches the state
    to scanning. Scanning currently only calculates the half width of
    angular bounding box.

--- 3d_scanner/3d_scanner.ino
+++ 3d_scanner/3d_scanner.ino
@@ -2,43 +2,62 @@
 // sensor. A finite state machine is used to control the state of the scanner and 
 
 #include <Servo.h>
+#include <math.h>
 Servo pan_servo;  // create servo object for the pan servo
 Servo tilt_servo; // create servo object for the tilt servo
 
+const uint8_t RED_LED = 5;
+const uint8_t GREEN_LED = 6;
+const uint8_t YELLOW_LED = 11;
 const uint8_t TILT_SERVO = 9;
 const uint8_t PAN_SERVO = 10;
 const uint8_t INFRARED_SENSOR = 0;
+const uint8_t SWITCH1 = 8;
+uint32_t current_time;
+uint32_t last_button_read;
+bool debounce_flag;
+uint32_t prior_time;
+float t = 0;
+float out;
 
 // User parameters of the 3D scanner and letter
-const uint8_t scanner_distance = 0.20; // Distance from the sensor to the letter (m)
-const uint8_t sensor_height = 0.1; // Distance from the sensor to the ground (m)
-const uint8_t letter_height = 0.3556; // Height of the letter to be scanned (m)
-const uint8_t letter_width = 0.2286; // Width of the letter to be scanned (m)
-const uint8_t letter_tolerance = 0.05; // Tolerance to measure to the sides of the letter (m)
-const uint8_t scanning_interval = 10; // Interval to wait between servo moves and scans (milliseconds)
+const float scanner_distance = 0.001; // Distance from the sensor to the letter (m)
+const float sensor_height = 0.1; // Distance from the sensor to the ground (m)
+const float letter_height = 0.3556; // Height of the letter to be scanned (m)
+const float letter_width = 0.2286; // Width of the letter to be scanned (m)
+const float letter_tolerance = 0.05; // Tolerance to measure to the sides of the letter (m)
+const uint16_t scanning_interval = 10; // Interval to wait between servo moves and scans (milliseconds)
 const uint16_t DEBOUNCE_INTERVAL = 10; // Minimum interval to wait before checking button presses 
+float scanning_width_angle; // 
 
 // User settings of the 3D scanner
-const uint8_t scanning_resolution = 1 // Angular resolution of the scanner (degrees)
+const uint8_t scanning_resolution = 1; // Angular resolution of the scanner (degrees)
 
 // Define states for the 3D scanner
 enum states {
   NONE,
-  READY,
+  IDLE,
   SCANNING
-}
+};
 
 // Define relative states
-states piror_state, state;
+states prior_state, state;
 
 void calculate_bounding_box() {
   // Based on the reading of the IMU and the height and distance of the sensor
   // from the letter, calculate the angular bounding box to scan.
   // Get absolute pitch, roll, and yaw from the IMU
-  float scanning_height = letter_height
+  float scanning_height = letter_height + 2 * letter_tolerance;
+  Serial.println(letter_height);
+  float scanning_width = letter_width + 2 * letter_tolerance;
+  Serial.println(scanning_width);
+
+  // Calculate the angles corresponding to half of the scanning range
+  float scanning_width_angle = (180*atan((scanning_width/2)/scanner_distance))/PI;
+  Serial.println(scanning_width_angle);
 }
 
-void ready() {
+void idle() {
   // Initialization for scanning state
   if (state != prior_state) {
     prior_state = state;
@@ -47,9 +66,29 @@ void ready() {
   // Wait for button press before scanning
   current_time = millis(); // Get current time for checking state transisions
 
+  if (current_time - prior_time > 10 ){
+  
+    t += 0.0005;
+    out = sin(t)* 127.5 + 127.5;
+  }
+
+  analogWrite(RED_LED, out);   
+  analogWrite(GREEN_LED, out);
+  analogWrite(YELLOW_LED, out);
+
   // Check for state transisions
   if (current_time >= last_button_read + DEBOUNCE_INTERVAL) { // Guard state transition behind if statment to counteract debounce
-    
+    if (digitalRead(SWITCH1) == HIGH && debounce_flag) {
+      state = SCANNING;
+      digitalWrite(RED_LED, LOW);   
+      digitalWrite(GREEN_LED, LOW);
+      digitalWrite(YELLOW_LED, LOW);
+      debounce_flag = false;
+    }
+    if (digitalRead(SWITCH1) == LOW && !debounce_flag) {
+      debounce_flag = true;
+    }
+    last_button_read = millis();
   }
 }
 
@@ -64,18 +103,29 @@ void scanning() {
 }
 
 void setup() {
-  // Define inputs and outputs
-  pan_servo.attach(5);
-  tilt_servo.attach(6);
+  Serial.begin(9600);
+  
+  // Define servos, inputs, and outputs
+  pan_servo.attach(PAN_SERVO);
+  tilt_servo.attach(TILT_SERVO);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(SWITCH1, INPUT);
 
   prior_state = NONE;
-  state = READY;
+  state = SCANNING; //TODO: Change to "IDLE" upon code completion
 
-  uint32_t last_button_read  = 0; // Last time the button was read as pressed. Set to 0 on setup. #TODO: Confirm if variable def this way good practice
+  last_button_read  = 0; // Last time the button was read as pressed. Set to 0 on setup. #TODO: Confirm if variable def this way good practice
+  prior_time = 0;
+  debounce_flag = true;
 }
 
 void loop() {
   switch (state) { // This looped section is the core of the state machine.
+    case IDLE:
+      idle();
+      break;
     case SCANNING:
       scanning();
       break;
